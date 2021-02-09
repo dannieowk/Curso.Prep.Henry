4b708c40be08110e016820d15334ac54d3c9691b
Daniel Gil <danniegil@hotmail.com>
2021-02-08 23:23:55 -0500

    Homework 02-JS - CONCEPTOS_BASICO

---
+++ 02-JS-I/homework/Conceptos.js
@@ -0,0 +1,15 @@
+// DEFINICIÒN DE CONCEPTOS BÀSICOS.
+// --> VARIABLES: Son elementos que pueden contener cualquier cosa, desde una letra hasta un numero.
+
+// --> STRING: Son elementos que pueden contener unicamente caracteres de texto.
+
+// --> FUNCIONES (Argumentos , return): Las funciones son bloques de codigos nos permiten ejecutar 
+// acciones de una manera màs rapida y eficiente.
+// Esto a traves de ARGUMENTOS que vendrìan siendo los valores o el alimento a evaluar en la ejecuciòn
+// de cada funcion obteniendo dicho valor o solucion en modo retorno.
+
+//DECLARACIONES IF: Es una forma que nos permite evaluar una condicion y ejecutar una accion ya sea 
+// esta accion sea falsa o verdadera. En formas mas complejas es posible realizar expresiones condicionales incluso anidando otros verificadores de condicion, es decir un IF dentro de otro IF.
+
+//VALORES BOLLEANOS: Son aquellos datos que solo pueden tomar como valor verdadero(true) o falso(false) o 
+// tambien 1 o 0.
