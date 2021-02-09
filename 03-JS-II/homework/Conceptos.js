5ec6a254ec67180ce7cb7168b9c4061521b4f682
Daniel Gil <danniegil@hotmail.com>
2021-02-09 16:45:53 -0500

    Conceptos 03-JS-II

---
+++ 03-JS-II/homework/Conceptos.js
@@ -0,0 +1,8 @@
+// DEFINICIÒN DE CONCEPTOS BÀSICOS.
+// --> FOR: Es un bucle o un modo de repetir un bloque de codigo de manera eficiente la cantidad de veces que deseemos segun condiciones.
+
+// --> &&: Es un operador logico que nos permite evaluar dos o mas condiciones teniendo en cuenta que cada una de estas sea verdadera (TODAS), si una de todas las condiciones condiciones es falsa entonces el resultado final sera falso.
+
+// --> ||: Es un operador logico que nos permite evaluar dos o mas condiciones teniendo en al menos una sea verdadera (AL MENOS UNA), si una de todas las condiciones se cumple, el resultado sera TRUE sin importar el resultado de las demas.
+
+
