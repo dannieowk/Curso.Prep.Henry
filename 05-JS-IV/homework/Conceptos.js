96d755689986217f863a843654fb5f06f0890696
Daniel Gil <danniegil@hotmail.com>
2021-02-12 00:24:53 -0500

    Conceptos.js

---
+++ 05-JS-IV/homework/Conceptos.js
@@ -0,0 +1,13 @@
+// CONCEPTOS
+
+// OBJETOS:  Es un conjunto de propiedades similar a los array pero estos no tienen indices numericos.
+
+// PROPIEDADES: Es una asociacion de una clave con un valor.
+
+// METODOS: Son  funciones guaradades en propiedades o basicamente funciones que representan un valor de una clave.
+
+// BUCLE FOR…IN: Es un bucle que nos permite iterar teniendo en cuenta la clave y el valor de un objeto.
+
+// NOTACION DE PUNTOS VS NOTACION DE CORCHETES: Con la notacion de puntos podemos llamar una clave de un objeto de 
+        // manera mas rapida que con la notacion de corchetes ya que implica digitar mas texto, incluyendo los parentesis mas 
+        // las comillas para poder llamar la clave del objeto
